e69591d93d12bdf1685efebb598b9faf98a2f0cd
shibatea <ggl.shibata@gmail.com>
2020-05-05 23:00:21 +0900

    feat: Add DataAccess Models

---
+++ ShoppingCart/ShoppingCart.DataAccess/Model/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ShoppingCart.DataAccess.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<ProductCategory> Products { get; set; }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.DataAccess/Model/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.DataAccess.Model
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public Product Product { get; set; }
+        public Category Category { get; set; }
+    }
+}
