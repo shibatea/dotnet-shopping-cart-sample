225a1b3a0c16f0a3cd0c542eaa568357cddc7363
shibatea <ggl.shibata@gmail.com>
2020-05-06 01:15:41 +0900

    feat: Implements Repository

--- ShoppingCart/ShoppingCart.Service/Repositories/CategoryRepo.cs
+++ ShoppingCart/ShoppingCart.Service/Repositories/CategoryRepo.cs
@@ -1,39 +1,48 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using ShoppingCart.DataAccess.Model;
+using ShoppingCart.Service.Data;
 using ShoppingCart.Service.Infrastructure;
 
 namespace ShoppingCart.Service.Repositories
 {
     public class CategoryRepo : ICategory
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public List<Category> GetAllCategories()
         {
-            throw new System.NotImplementedException();
+            return _context.Categories.ToList();
         }
 
         public Category GetCategoryById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.Categories.FirstOrDefault(x => x.Id == id);
         }
 
         public void InsertCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _context.Categories.Add(category);
         }
 
         public void UpdateCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _context.Categories.Update(category);
         }
 
         public void DeleteCategory(Category category)
         {
-            throw new System.NotImplementedException();
+            _context.Categories.Remove(category);
         }
 
         public void Save()
         {
-            throw new System.NotImplementedException();
+            _context.SaveChanges();
         }
     }
 }
---
+++ ShoppingCart/ShoppingCart.Service/Repositories/ProductRepo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.DataAccess.Model;
+using ShoppingCart.Service.Data;
+using ShoppingCart.Service.Infrastructure;
+
+namespace ShoppingCart.Service.Repositories
+{
+    public class ProductRepo : IProduct
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _context.Products
+                .Include(product => product.Categories)
+                .ThenInclude(productCategory => productCategory.Category)
+                .FirstOrDefault(product => product.Id == id);
+        }
+
+        public void InsertProduct(Product product)
+        {
+        }
+
+        public void UpdateProduct(Product product)
+        {
+        }
+
+        public void DeleteProduct(Product product)
+        {
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
