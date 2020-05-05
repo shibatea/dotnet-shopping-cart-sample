4ae244b5d5ea924484bf9c67a48a1c02acdb11cb
shibatea <ggl.shibata@gmail.com>
2020-05-05 23:07:00 +0900

    feat: Add Category Repository

---
+++ ShoppingCart/ShoppingCart.Service/Repositories/CategoryRepo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using ShoppingCart.DataAccess.Model;
+using ShoppingCart.Service.Infrastructure;
+
+namespace ShoppingCart.Service.Repositories
+{
+    public class CategoryRepo : ICategory
+    {
+        public List<Category> GetAllCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertCategory(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
