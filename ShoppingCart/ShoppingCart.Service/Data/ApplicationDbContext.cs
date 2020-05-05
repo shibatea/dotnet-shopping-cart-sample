419cb7c13040d3d4cc5135a58e0cd9c2417a8235
shibatea <ggl.shibata@gmail.com>
2020-05-05 23:11:47 +0900

    feat: Add ApplicationDbContext

---
+++ ShoppingCart/ShoppingCart.Service/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.DataAccess.Model;
+
+namespace ShoppingCart.Service.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
