875569cf24f149a71bba5d3def9dd45c6a3480d2
shibatea <ggl.shibata@gmail.com>
2020-05-06 01:29:49 +0900

    feat: Add CategoriesContoroller

---
+++ ShoppingCart/ShoppingCart.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Service.Infrastructure;
+using ShoppingCart.Web.ViewModels;
+
+namespace ShoppingCart.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategory _category;
+
+        public CategoriesController(ICategory category)
+        {
+            _category = category;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var allCategories = _category.GetAllCategories();
+            var vm = allCategories
+                .Select(category => new CategoryViewModel
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                })
+                .ToList();
+            return View(vm);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
--- ShoppingCart/ShoppingCart.Web/Startup.cs
+++ ShoppingCart/ShoppingCart.Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCart.Service.Infrastructure;
+using ShoppingCart.Service.Repositories;
 
 namespace ShoppingCart.Web
 {
@@ -24,6 +26,8 @@ namespace ShoppingCart.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<ICategory, CategoryRepo>();
+            services.AddTransient<IProduct, ProductRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
